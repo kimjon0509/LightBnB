5996d7ceed7284161dce2768433127494ee394f5
John Kim <kimjon0509@gmail.com>
2020-05-28 16:29:54 +0000

    added where clause to getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -77,10 +77,74 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  let whereClause = false;
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    if (!whereClause) {
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+      whereClause = true;
+    } else {
+      queryString += `AND city LIKE $${queryParams.length} `
+    }
+  }
+
+  if (options.minimum_price_per_night || options.maximum_price_per_night) {
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night);
+      queryParams.push(options.maximum_price_per_night);
+      if (!whereClause) {
+        queryString += `WHERE cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length}  `;
+        whereClause = true;
+      } else {
+        queryString += `AND cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length}  `
+      }
+    } else if (options.minimum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night);
+      if (!whereClause) {
+        queryString += `WHERE cost_per_night > $${queryParams.length}  `;
+        whereClause = true;
+      } else {
+        queryString += `AND cost_per_night > $${queryParams.length}  `
+      }
+    } else if (options.maximum_price_per_night) {
+      queryParams.push(options.maximum_price_per_night);
+      if (!whereClause) {
+        queryString += `WHERE cost_per_night < $${queryParams.length}  `;
+        whereClause = true;
+      } else {
+        queryString += `AND cost_per_night < $${queryParams.length}  `
+      }
+    }
+  }
+
+  if(options.minimum_rating) {
+    queryParams.push(options.minimum_rating)
+    if (!whereClause) {
+      queryString += `WHERE rating > $${queryParams.length} `;
+      whereClause = true;
+    } else {
+      queryString += `AND rating > $${queryParams.length} `
+    }
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
